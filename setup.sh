456b2b39f216dee617a2e90aed6e59e8ad12c6ec
echo <cdixon99@ivytech.edu>
2020-04-22 20:39:25 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # copyleft 2020
 echo starting the setup sit back and relax...
-echo curent user is $USER
+echo "curent user is $USER"
+echo "user home is $HOME"
 echo "press ctrl-D to stop"
 echo "WARNING: pleae make sure this in you're in you're home directory otherwise things may break"
 echo "but dont wory this script should do it for you : ) "
