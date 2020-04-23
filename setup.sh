723255839ce2cda2d22b7f0d2981fb2c2b77d88b
echo <cdixon99@ivytech.edu>
2020-04-22 20:44:08 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ echo "user home is $HOME"
 echo "press ctrl-D to stop"
 echo "WARNING: pleae make sure this in you're in you're home directory otherwise things may break"
 echo "but dont wory this script should do it for you : ) "
-chmod +x pkget
 mv pget pget-dev
 mv pkget-dev $HOME
 mv $HOME/pget-dev pget
+chmod +x $HOME/pget
 echo this will only take a second
 echo modifying the PATH....
 echo export PATH="$PATH:/home/$USER/.apt/usr/bin" >> .bashrc
