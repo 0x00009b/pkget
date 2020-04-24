9e7ba136ef47488edcd85abd02b89055818157d5
echo <cdixon99@ivytech.edu>
2020-04-24 16:29:34 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # copyleft 2020
-echo starting the setup sit back and relax...
 echo "curent user is $USER"
 echo "user home is $HOME"
 echo "press ctrl-D to stop"
@@ -10,8 +9,6 @@ chmod +x pget
 mv pget $HOME
 echo this will only take a second
 echo modifying the PATH....
-echo export PATH="$PATH:/home/$USER/.apt/usr/bin" >> .profile
-echo export PATH="$PATH:/home/$USER/.apt/usr/bin" >> .bashrc
-echo PATH has been sent 
-echo $PATH
-echo this only works for bash 
+echo export PATH="$PATH:.apt/usr/bin" >> .profile
+echo export PATH="$PATH:.apt/usr/bin >> .bashrc
+echo "PATH has been set"
