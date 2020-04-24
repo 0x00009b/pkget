59d61943f92a69564e0ecadde29a246c12e48f71
echo <cdixon99@ivytech.edu>
2020-04-24 16:30:14 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@
 # copyleft 2020
 echo "curent user is $USER"
 echo "user home is $HOME"
-echo "press ctrl-D to stop"
-echo "WARNING: pleae make sure this in you're in you're home directory otherwise things may break"
 echo "but dont wory this script should do it for you : ) "
 chmod +x pget
 mv pget $HOME
